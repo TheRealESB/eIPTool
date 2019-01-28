e24313dc330c1bb6d3c1a89e01af777075342ffb
TheRealESB <46871959+TheRealESB@users.noreply.github.com>
2019-01-28 18:26:50 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+clear
+echo "
+███╗   ███╗ █████╗ ███╗   ██╗██╗███████╗███████╗ ██████╗
+████╗ ████║██╔══██╗████╗  ██║██║██╔════╝██╔════╝██╔═══██╗
+██╔████╔██║███████║██╔██╗ ██║██║███████╗███████╗██║   ██║
+██║╚██╔╝██║██╔══██║██║╚██╗██║██║╚════██║╚════██║██║   ██║
+██║ ╚═╝ ██║██║  ██║██║ ╚████║██║███████║███████║╚██████╔╝
+╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝╚══════╝╚══════╝ ╚═════╝ 
+▀▀█▀▀ █▀▀█ █▀▀█ █   █▀▀ ~ Tools Instaler By Ⓜ Ⓐ Ⓝ Ⓘ Ⓢ Ⓢ Ⓞ  ☪ ~
+  █   █  █ █  █ █   ▀▀█ 
+  ▀   ▀▀▀▀ ▀▀▀▀ ▀▀▀ ▀▀▀             
+
+                                                ";
+
+echo "[✔] Checking directories...";
+if [ -d "/usr/share/doc/IPTool" ] ;
+then
+echo "[◉] A directory Crips was found! Do you want to replace it? [Y/n]:" ; 
+read mama
+if [ $mama == "y" ] ; 
+then
+ rm -R "/usr/share/doc/IPTool"
+else
+ exit
+fi
+fi
+
+ echo "[✔] Installing ...";
+ echo "";
+ git clone https://github.com/TheRealESB/IPTool.git /usr/share/doc/IPTool;
+ echo "#!/bin/bash 
+ python /usr/share/doc/IPTool/iptool.py" '${1+"$@"}' > iptool;
+ chmod +x crips;
+ sudo cp crips /usr/bin/;
+ rm iptool;
+
+
+if [ -d "/usr/share/doc/Crips" ] ;
+then
+echo "";
+echo "[✔]Tool istalled with success![✔]";
+echo "";
+  echo "[✔]====================================================================[✔]";
+  echo "[✔] ✔✔✔  All is done!! You can execute tool by typing crips  !     ✔✔✔ [✔]"; 
+  echo "[✔]====================================================================[✔]";
+  echo "";
+else
+  echo "[✘] Installation faid![✘] ";
+  exit
+fi
